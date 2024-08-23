e8d9942b3d8a559bee788a1a2ff7973dee3d55b7
anwazkhan <anwazkhan932@gmail.com>
2024-08-23 11:21:20 +0530

    my first commit

---
+++ Dior/src/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+
+function Home () {
+    return(
+        <>
+       <div>
+        <div className='logo'>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Ddior%2BLogo&psig=AOvVaw1Dh0bSTKWZobbN3XNbJ9Ma&ust=1724420065414000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJC0-r3biIgDFQAAAAAdAAAAABAI" alt="" />
+            
+        </div>
+       </div>
+        
+        </>
+    )
+}
+
+export default Home;
